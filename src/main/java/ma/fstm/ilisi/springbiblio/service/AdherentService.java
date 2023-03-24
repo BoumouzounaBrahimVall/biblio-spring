20ecb3054c9773ea0d5a585e1813cef4d51a23fc
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-03-24 22:57:44 +0100

    service adherent done

--- src/main/java/ma/fstm/ilisi/springbiblio/service/AdherentService.java
+++ src/main/java/ma/fstm/ilisi/springbiblio/service/AdherentService.java
@@ -1,4 +1,33 @@
 package ma.fstm.ilisi.springbiblio.service;
 
+import ma.fstm.ilisi.springbiblio.bo.Adherent;
+import ma.fstm.ilisi.springbiblio.dao.AdherentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class AdherentService {
+    @Autowired
+    AdherentRepository repo;
+
+    public AdherentService(){}
+    public List<Adherent> listAll(){
+        return repo.findAll();
+    }
+    public void save(Adherent a){
+        repo.save(a);
+    }
+
+    public void delete(String id){
+        repo.deleteById(id);
+    }
+    public Optional<Adherent> get(String id){
+        return repo.findById(id);
+    }
+
 }
